9d7307dc7ac7359c522bb34ab1dc1a975d54f844
stone <Lei.Shi445@cqaos.com>
2019-12-11 20:38:28 +0800

    完成了分类功能的开发

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -43,7 +43,7 @@
           return this.$message.error('获取权限列表失败')
         }
         this.rightsList = res.data
-        console.log(this.rightsList)
+        // console.log(this.rightsList)
       }
     }
   }
